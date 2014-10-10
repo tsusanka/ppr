2deaf94824e2c7cbedf52f975d3b6c3c480311fa
Tomas Susanka <tsusanka@gmail.com>
2014-10-10 23:17:18 +0200

    Main: reading input - q & n

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <assert.h>
 #include "triangle.h"
 
 main()
 {
-	Triangle *t = new Triangle(5);
+	int n = 0, q = 0;
+	printf("Enter triangle size (n):");
+	scanf("%d", &n);
+	assert(n > 0);
+	printf("Enter random steps count (q):");
+	scanf("%d", &q);
+	assert(q > 0);
+
+	Triangle *t = new Triangle(n);
 	t->fill();
 	t->print();
 }
