3b04a2a2cbb87ab6e2619bcee15a1821ee90f426
Tomas Susanka <tsusanka@gmail.com>
2014-11-24 09:58:50 +0100

    typos and fixes

--- main.cpp
+++ main.cpp
@@ -40,104 +40,116 @@ void copySolution( Direction * where, Node * from )
 	while (node != NULL);
 }
 
-Direction * getPath(Node * lastNode){
-        Direction * result = new Direction[LENGTH];
-    	int i = 0;
+Direction * getPath(Node * lastNode)
+{
+    Direction * result = new Direction[LENGTH];
+    int i = 0;
 	do
 	{
 		result[i++] = lastNode->direction;
 		lastNode = lastNode->prevNode;
 	}
 	while (lastNode != NULL);
-        result[i] = NONE;
-        return result;
+    result[i] = NONE;
+    return result;
 }
 
-void fillStackFromMessage( Stack * s, Triangle * t, char * message ){
-    	int position = 0;
-	int number;
-        Direction direction;
-        Node * lastNode;
-        int i = 1;
-        while(true)
-        {
-                MPI_Unpack(message, LENGTH, &position, &number, 1, MPI_INT, MPI_COMM_WORLD);
-                direction = (Direction) number;
-                if( direction == NONE ){
-                    break;
-                }
-                int result = t->move ( direction ); 
-                if( result == -1 ){
-                    printf("fillStackFromMessage>Invalid MOVE recieved:%d",number);
-                }
-                Node * n = new Node(lastNode, direction, i++);
-                n->direction = direction;
-                n->prevNode = lastNode;
-                n->steps = i++;
-                
-                lastNode = n;
-        }
-        t->move( t->oppositeDirection(lastNode->direction) ); //revert the last move, becasuse it will be done after it is popped from the stack
-        s->push(lastNode);
-}
-
-void sendWork(Stack * s, Triangle * t, int to, Node * lastNode )
+void fillStackFromMessage( Stack * s, Triangle * t, char * message )
 {
-  //SEND THIS NODE TO PROCESSOR P
+    int position = 0;
+	int number;
+    Direction direction;
+    Node * lastNode;
+    int i = 1;
+    while(true)
+    {
+        MPI_Unpack(message, LENGTH, &position, &number, 1, MPI_INT, MPI_COMM_WORLD);
+        direction = (Direction) number;
+        if( direction == NONE )
+        {
+            break;
+        }
+        int result = t->move ( direction );
+        if( result == -1 )
+        {
+            printf("fillStackFromMessage>Invalid MOVE recieved:%d",number);
+        }
+        Node * n = new Node(lastNode, direction, i++);
+        lastNode = n;
+    }
+    t->move( t->oppositeDirection(lastNode->direction) ); //revert the last move, because it will be done after it is popped from the stack
+    s->push(lastNode);
+}
+
+/**
+ * Sends node to defined processor
+ */
+void sendWork(int to, Node * lastNode )
+{
     char * buffer = new char[LENGTH];
     int position = 0;
     Direction * result = getPath(lastNode);
     int ri = 0;
     do
     {
-            int a = (int) result[ri++];
-            MPI_Pack(&a, 1, MPI_INT, buffer, LENGTH, &position, MPI_COMM_WORLD);
-    }while( result[ri] != NONE);
-    MPI_Send( (void*) buffer, position, MPI_PACKED, to, MSG_WORK_SENT, MPI_COMM_WORLD );   
+        int a = (int) result[ri++];
+        MPI_Pack(&a, 1, MPI_INT, buffer, LENGTH, &position, MPI_COMM_WORLD);
+    }
+    while( result[ri] != NONE);
+    MPI_Send( (void*) buffer, position, MPI_PACKED, to, MSG_WORK_SENT, MPI_COMM_WORLD );
 }
 
-int workState( Stack * s, int toInitialSend, Triangle * t, int myRank ) {
+int workState( Stack * s, int toInitialSend, Triangle * t, int myRank, int bestCount )
+{
     int checkMsgCounter = 0;
     int tag = 1;
     int flag;
-    MPI_Status status;
     char message[LENGTH];
-    
-    if( myRank != 0) {
-        while{
+    MPI_Status status;
+    Direction * bestSolution = new Direction[bestCount];
+    Node * lastNode = NULL;
+
+    // the first work ever that is sent to other processors
+    if( myRank != 0)
+    {
+        while (true)
+        {
             MPI_Iprobe(0, MSG_WORK_SENT, MPI_COMM_WORLD, &flag, &status);
             if( flag ) break;
         }
         MPI_Recv( message, LENGTH, MPI_PACKED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status );
-        //unpack data
-        fillStackFromMessage( s, t, message);
+        fillStackFromMessage(s, t, message);
     }
-    
+
     int sendWorkTo = -1;
     // ======== DEPTH-FIRST SEARCH ==========//
-    
+
     while( s->getSize() > 0 )
 	{   
-                if ((checkMsgCounter++ % CHECK_MSG_AMOUNT)==0)
+        if ((checkMsgCounter++ % CHECK_MSG_AMOUNT) == 0) // TODO: nehrozi ze pretece checkMsgCounter?
+        {
+            MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
+            if (flag)
+            {
+                //prisla zprava, je treba ji obslouzit
+                //v promenne status je tag (status.MPI_TAG), cislo odesilatele (status.MPI_SOURCE)
+                //a pripadne cislo chyby (status.MPI_ERROR)
+                switch (status.MPI_TAG)
                 {
-                        MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
-                        if (flag)
-                        {
-                                //prisla zprava, je treba ji obslouzit
-                                //v promenne status je tag (status.MPI_TAG), cislo odesilatele (status.MPI_SOURCE)
-                                //a pripadne cislo chyby (status.MPI_ERROR)
-                                switch (status.MPI_TAG)
-                                {
-                                   case MSG_WORK_REQUEST : // send work
-                                                            sendWorkTo = status.MPI_SOURCE;
-                                                           break;
-                                   case MSG_TOKEN : // send black token
-                                                    break;
-                                   default : printf("neznamy typ zpravy!\n"); break;
-                              }
-                        }
-                }
+                   case MSG_WORK_REQUEST : // send work
+                                            sendWorkTo = status.MPI_SOURCE;
+                                           break;
+                   case MSG_TOKEN : // TODO send black token
+                                    break;
+                   default : printf("neznamy typ zpravy!\n"); break;
+              }
+            }
+        }
 		Node* n = s->pop();
+        if (lastNode == NULL)
+        {
+            lastNode = n->prevNode; // ocheck
+        }
 
 		if( DEBUG )
 		{
@@ -181,7 +193,7 @@ int workState( Stack * s, int toInitialSend, Triangle * t, int myRank ) {
 
 		if( t->isSorted() ) // this is a solution
 		{
-			printf("Sorted! Steps: %d; bestCount: %d\n", n->steps, bestCount);
+			printf("Sorted! Steps: %d; bestCount: %d\n", n->steps, bestCount); // TODO: send bestCount to other processors
 			if( n->steps <= bestCount )
 			{
 				bestCount = n->steps;
@@ -195,20 +207,25 @@ int workState( Stack * s, int toInitialSend, Triangle * t, int myRank ) {
 
 		if( n->steps < bestCount )
 		{
-                        if( toInitialSend > 0 ){
-                            sendWork(s, t, toInitialSend, n);
-                            toInitialSend--;
-                        }else if( sendWorkTo != -1){
-                            sendWork(s, t, sendWorkTo, n);
-                            sendWorkTo = -1;
-                        }else{
-                            if( DEBUG ) printf("inserting moves\n");
-                            for ( int dir = TOP_LEFT; dir <= BOTTOM_RIGHT; dir++ )
-                            {
-                                    Direction direction = Direction(dir);
-                                    s->push( new Node(n, direction, n->steps + 1 ));
-                            }
-                        }
+            if( toInitialSend > 0 )
+            {
+                sendWork(toInitialSend, n);
+                toInitialSend--;
+            }
+            else if( sendWorkTo != -1)
+            {
+                sendWork(sendWorkTo, n);
+                sendWorkTo = -1;
+            }
+            else
+            {
+                if( DEBUG ) printf("inserting moves\n");
+                for ( int dir = TOP_LEFT; dir <= BOTTOM_RIGHT; dir++ )
+                {
+                    Direction direction = Direction(dir);
+                    s->push( new Node(n, direction, n->steps + 1 ));
+                }
+            }
 		}
 		else
 		{
@@ -221,53 +238,58 @@ int workState( Stack * s, int toInitialSend, Triangle * t, int myRank ) {
     return IDLE;
 }
 
-int idleState(Stack * s, Triangle * t, int myRank, int numberOfProcessor){
-     int tag = 1;
+int idleState(Stack * s, Triangle * t, int myRank, int numberOfProcessor)
+{
     int flag;
     MPI_Status status;
     char message[LENGTH];
-    
     int attempts = 0;
     int sent = 0;
-    while(true){
-        if( !sent) {
-            
+    int position = 0; // ocheck
+
+    while(true)
+    {
+        if( !sent )
+        {
             int dest;
-            do{
-                dest = rand() % numberOfProcessor;
-            } while( dest != myRank);
-            MPI_Send( (void*) NULL, position, MPI_CHAR, dest , MSG_WORK_REQUEST, MPI_COMM_WORLD );
+            do
+            {
+                dest = rand() % numberOfProcessor; // generating destination randomly except to yourself
+            }
+            while( dest != myRank);
+            MPI_Send( (void*) NULL, position, MPI_CHAR, dest, MSG_WORK_REQUEST, MPI_COMM_WORLD ); // ocheck
             sent = 1;
         }
-           MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
-            if (flag)
+        MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
+        if (flag)
+        {
+            //prisla zprava, je treba ji obslouzit
+            //v promenne status je tag (status.MPI_TAG), cislo odesilatele (status.MPI_SOURCE)
+            //a pripadne cislo chyby (status.MPI_ERROR)
+            switch (status.MPI_TAG)
             {
-                    //prisla zprava, je treba ji obslouzit
-                    //v promenne status je tag (status.MPI_TAG), cislo odesilatele (status.MPI_SOURCE)
-                    //a pripadne cislo chyby (status.MPI_ERROR)
-                    switch (status.MPI_TAG)
+                case MSG_WORK_SENT : //  accept work and switch to workState
+                    MPI_Recv( message, LENGTH, MPI_PACKED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status );
+                    fillStackFromMessage(s, t, message);
+                    return WORK;
+                case MSG_WORK_NOWORK : // ask some other cpu for work, or if attemps == numberOfProcessors-1 and myrank == 0 sent white token
+                    attempts++;
+                    if( attempts >= numberOfProcessor-1)
                     {
-                       case MSG_WORK_SENT : //  accept work and switch to workState
-                                            MPI_Recv( message, LENGTH, MPI_PACKED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status );
-                                            fillStackFromMessage(s, t, message);
-                                            return WORK;
-                       case MSG_WORK_NOWORK : // ask some other cpu for work, or if attemps == numberOfProcessors-1 and myrank == 0 sent white token
-                                                attempts++;
-                                                if( attempts >= numberOfProcessor-1){
-                                                    if (myRank == 0)
-                                                    {
-                                                        return TOKEN;
-                                                    }
-                                                }
-                                                sent = 0;
-                                               break;
-                       case MSG_FINISH : // finish, switch to finish state
-                                                return FINISH;
-                       case MSG_TOKEN : // if token is black send black else send white token to cpu+1, if myrank==0 and token is white send finish and switch to finish state
-                                        break;
-                       default : printf("neznamy typ zpravy!\n"); break;
-                  }
+                        if (myRank == 0)
+                        {
+                            return TOKEN;
+                        }
+                    }
+                    sent = 0;
+                    break;
+                case MSG_FINISH : // finish, switch to finish state
+                                        return FINISH;
+                case MSG_TOKEN : // if token is black send black else send white token to cpu+1, if myrank==0 and token is white send finish and switch to finish state
+                                break;
+                default : printf("neznamy typ zpravy!\n"); break;
             }
+        }
     }
 }
 
@@ -283,7 +305,7 @@ int main( int argc, char** argv )
     srand(time(NULL));
 
 	/* MPI VARIABLES */
-	int my_rank;
+	int myRank;
 	int numberOfProcessors;
 	int tag = 1;
 	int flag;
@@ -294,7 +316,7 @@ int main( int argc, char** argv )
 	MPI_Init(&argc, &argv);
 
 	/* find out process rank */
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
 	/* find out number of processes */
 	MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcessors);
@@ -325,9 +347,11 @@ int main( int argc, char** argv )
 	}
 	assert(q > 0);
 
-	printf("Hello i am CPU #%d. q is %d, n is %d \n", my_rank, q, n);
+    bestCount = q;
 
-	if( my_rank == 0 )
+	printf("Hello i am CPU #%d. q is %d, n is %d \n", myRank, q, n);
+
+	if( myRank == 0 )
 	{
 		printf("There are %d processors. \n", numberOfProcessors);
 		//INIT ARGS AND TRIANGLE AND SEND TO OTHER PROCESSES
@@ -353,17 +377,15 @@ int main( int argc, char** argv )
 			MPI_Send( (void*) message, position, MPI_PACKED, destination, tag, MPI_COMM_WORLD );
 			destination++;
 		}
-
-		bestCount = q;
 	}
 	else
 	{
 		MPI_Status status;
 		int flag = 0;
-		//WAIT FOR SHUFFLED TRIANGLE
+		// WAIT FOR SHUFFLED TRIANGLE
 		while (!flag)
 		{
-			MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status); //busy waiting(?)
+			MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status); // busy waiting(?)
 		}
 
 		t = new Triangle(n);
@@ -372,52 +394,49 @@ int main( int argc, char** argv )
 		MPI_Recv( message, LENGTH, MPI_PACKED, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status );
 		t->unpack(message);
 
-		printf("proc #%d", my_rank);
-		printf("\n");
-
-		// parse message
-		t = 0; // PARSE TRIANGLE
-		bestCount = 8;// PARSE NUMBER OF SHUFFLES;
+        if ( DEBUG )
+        {
+            printf("%d: triangle:", myRank);
+            t->print();
+            printf("\n");
+        }
 	}
-	/*usleep(microseconds);
-	MPI_Finalize();
-	return 0;*/
+
 	Stack * s = new Stack;
 
-        
-        int toInitialSend = 0;
-        if( my_rank == 0 ){
-            toInitialSend = numberOfProcessors - 1;
-            // first nodes inserted to stack
-            for ( int dir = TOP_LEFT; dir <= BOTTOM_RIGHT; dir++ )
-            {
-                    Direction direction = Direction(dir);
-                    Node * initialNode = new Node(NULL, direction, 1);
-                    s->push( initialNode );
-            }
+    int toInitialSend = 0;
+    if( myRank == 0 )
+    {
+        toInitialSend = numberOfProcessors - 1;
+        // first nodes inserted to stack
+        for ( int dir = TOP_LEFT; dir <= BOTTOM_RIGHT; dir++ )
+        {
+            Direction direction = Direction(dir);
+            Node * initialNode = new Node(NULL, direction, 1);
+            s->push( initialNode );
         }
-	 
-	Direction * bestSolution = new Direction[bestCount];
+    }
 
 	Node * lastNode = new Node(NULL, RIGHT, 1);
-        int nextState = WORK;
-        do{
-            switch (nextState)
-            {
-                case WORK:
-                    nextState = workState(s, toInitialSend, t, my_rank);
-                    break;
-                case IDLE:
-                    nextState = idleState(s, t, my_rank, numberOfProcessors);
-                    break;
-                case TOKEN:
-                    nextState = tokenState(s, t);
-                    break;
-            }
-        }while( nextState != FINISH )
-	
+    int nextState = WORK;
+    do{
+        switch (nextState)
+        {
+            case WORK:
+                nextState = workState(s, toInitialSend, t, myRank, bestCount);
+                break;
+            case IDLE:
+                nextState = idleState(s, t, myRank, numberOfProcessors);
+                break;
+            case TOKEN:
+                nextState = tokenState(s, t);
+                break;
+        }
+    }
+    while( nextState != FINISH );
 
-	
+
+    // TODO gather all results
 
 	printf("==============================\n");
 	printf("End: best solution found with %d steps. Moves:\n", bestCount);
